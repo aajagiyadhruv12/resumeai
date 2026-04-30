f80043853d7d0168bece79f4ebc63e59a93939ff
aajagiyadhruv12 <aajagiyadhruv12@gmail.com>
2026-04-30 11:53:33 +0530

    Updates

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOMClient from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
-const root = ReactDOMClient.createRoot(document.getElementById('root'));
-root.render(<App />);
